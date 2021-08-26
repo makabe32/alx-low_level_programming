1fb8be2034ca2d3f346468e615f0c481b4501aac
makabe32 <morrismakabe32@gmail.com>
2021-08-26 13:23:10 -0700

    prints a message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 # include <stdio.h>
+
 /**
  *This is a new comment
  *Making a file that prints a message
  */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
