4c466458bdbd332d8b9a44c3a13f0f71148aed73
makabe32 <morrismakabe32@gmail.com>
2021-08-26 13:48:05 -0700

    prints message

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,13 @@
-/*This is a description of my function.*/
 #include <stdio.h>
+
+/**
+ * main - strings printed by printf
+ * followed by newline
+ * Return: alway 0 on (success)
+ */
+
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-/*This is another comment*/
 return (0);
 }
