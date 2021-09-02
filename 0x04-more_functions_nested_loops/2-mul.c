6240d1f5eab185a99c996c75a4be5528e2f0eb18
makabe32 <morrismakabe32@gmail.com>
2021-09-02 11:34:50 -0700

    commits again

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(int a, int b)
+{
+  int a, int b, mul;
+
+  int a = 98;
+  int b = 1024;
+  
+  mul = int a * int b
+    
+    printf("%d\n", mul(98, 1024));
+;
+
+    return (0);
+
+}
