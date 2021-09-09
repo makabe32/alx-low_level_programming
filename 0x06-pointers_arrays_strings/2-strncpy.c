f4622b9f47c8f293f6e4600bac701761acc7241a
makabe32 <morrismakabe32@gmail.com>
2021-09-09 12:14:42 -0700

    rectified-function that copies a string.

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,5 +1,13 @@
-#include "main.h"
+#include "holberton.h"
 
+/**
+ * _strncpy - copies a string
+ * @dest: destination string
+ * @src: source string
+ * @n:  bytes to copy
+ *
+ * Return
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
